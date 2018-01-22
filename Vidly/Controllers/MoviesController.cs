71b2cd9ffbdf4b072236081415893ed76804d5ef
Kishan N <kishann380@gmail.com>
2018-01-22 14:24:30 +0530

    Conventional Routing, Added Bootstrap files, Added Model movies

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Padmavat" };
+            //return Content("Hello World!");
+            //return new HttpNotFoundResult();
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index","Home",new { page = 1, sortBy = "name" });
+            return View(movie);
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("MovieId=" + movieId);
+        }
+
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if(String.IsNullOrEmpty(sortBy))
+            {
+                sortBy = "name";
+            }
+            return Content(String.Format("Page Index = {0} SortBy {1}",pageIndex,sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year=2018,int month=01)
+        {
+
+            return Content(year+"/"+month);
+        }
+    }
+    
+}
